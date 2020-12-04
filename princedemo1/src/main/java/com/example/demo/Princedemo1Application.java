72adc87ab0841b23c75f248419a4f4c131fe6279
rachit.bishnoi <rachit.bishnoi@BLRKEC330490L.ad.infosys.com>
2020-12-04 23:07:03 +0530

    Second commit

--- princedemo1/src/main/java/com/example/demo/Princedemo1Application.java
+++ princedemo1/src/main/java/com/example/demo/Princedemo1Application.java
@@ -1,13 +1,25 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+
+@Controller
 @SpringBootApplication
 public class Princedemo1Application {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String hello() {
+		return "Hello World from e4developer!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Princedemo1Application.class, args);
 	}
-
 }
